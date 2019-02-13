77abc5548fe025ce58c0a5c0dbdb00a1bfac9546
Renaud Tertrais <renaud.tertrais@gmail.com>
2019-02-14 00:00:21 +0100

    Updates

--- README.md
+++
@@ -1,9 +0,0 @@
-# My resume
-
-This project is my resume made with yaml data injected in React.
-
-See result here: https://renaudtertrais.github.io/resume/
-
-## Licence
-
-MIT © 2016-present Renaud Tertrais
---
+++ precache-manifest.7fc63039feeaa1b70a12a1275ae60f96.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/resume/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "dab3e0419a9a874bdbd1",
+    "url": "/resume/static/css/main.26e928c0.chunk.css"
+  },
+  {
+    "revision": "fd78f13f2eeb6b64bd90",
+    "url": "/resume/static/js/runtime~main.fd78f13f.js"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/resume/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b75d643a686bcf5f30a2",
+    "url": "/resume/static/js/2.b75d643a.chunk.js"
+  },
+  {
+    "revision": "dab3e0419a9a874bdbd1",
+    "url": "/resume/static/js/main.dab3e041.chunk.js"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/resume/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/resume/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/resume/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "b75d643a686bcf5f30a2",
+    "url": "/resume/static/css/2.a5111124.chunk.css"
+  },
+  {
+    "revision": "0c201f0e405062f1abb7564ff89bc01c",
+    "url": "/resume/index.html"
+  }
+];
