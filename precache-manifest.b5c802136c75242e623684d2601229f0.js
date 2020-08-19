34bd6f922ff19b9f55284dd4cb398f6d8c282d66
Renaud Tertrais <renaud.tertrais@gmail.com>
2020-08-19 18:20:46 +1000

    Updates

---
+++ precache-manifest.b5c802136c75242e623684d2601229f0.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85edaf552ba8464b773e8b9a2b4268d6",
+    "url": "/resume/index.html"
+  },
+  {
+    "revision": "2093d1c10295d5d7193f",
+    "url": "/resume/static/css/2.fbeba37a.chunk.css"
+  },
+  {
+    "revision": "d7cd0edb8549451840a1",
+    "url": "/resume/static/css/main.da19f761.chunk.css"
+  },
+  {
+    "revision": "2093d1c10295d5d7193f",
+    "url": "/resume/static/js/2.f70f9bd1.chunk.js"
+  },
+  {
+    "revision": "241e2720c0228744c712295a835ee806",
+    "url": "/resume/static/js/2.f70f9bd1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d7cd0edb8549451840a1",
+    "url": "/resume/static/js/main.ed2bbf1f.chunk.js"
+  },
+  {
+    "revision": "c9e1e60fdeba81c21584",
+    "url": "/resume/static/js/runtime-main.01c6ff87.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/resume/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/resume/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/resume/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/resume/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/resume/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "fff77be4c3980e3509db6cceb27e7730",
+    "url": "/resume/static/media/renaud-tertrais.fff77be4.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/resume/precache-manifest.32e7cb08fdd49a5d83fd6c96a3201894.js"
+  "/resume/precache-manifest.b5c802136c75242e623684d2601229f0.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/resume/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/resume/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
